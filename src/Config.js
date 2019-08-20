0c1049503e5fe7fde0d0db5a76b31c3afa2cc93e
a2741890 <44138757+a2741890@users.noreply.github.com>
2019-08-20 23:43:14 +1000

    test

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  appId: '6b2cde72-fc43-4759-bb16-463fab5a0f68',
+  scopes: [
+    "user.read",
+    "calendars.read",
+    "calendars.readwrite",
+    "Bookings.ReadWrite.All"
+  ]
+};
