7512887ba4c0aa4885fefe0d84cc3345ca5cf0c5
a2741890 <44138757+a2741890@users.noreply.github.com>
2019-10-11 20:21:51 +1000

    Calendar implemented
    1. Calendar interface
    2. list events
    3. creat event

--- server.js
+++ server.js
@@ -4,118 +4,186 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var cors = require('cors');
+const fetch = require("node-fetch");
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-// 增加 body 解析
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}))
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-
-
-app.get('/accessToken', function(req,res){
-  res.header("Access-Control-Allow-Origin", "*");
-  var last_timestamp;
-  var current_timestamp = Date.parse(new Date());
-  var access_token;
-// 建立連線
-var conn = mysql.createConnection({
-  host : '127.0.0.1',
-  user : 'root',
-  password : 'a2370307',
-  database : 'IFN702'
-  });
-  // 建立連線後不論是否成功都會呼叫
-  conn.connect(function(err){
-  if(err) throw err;
-  console.log('connect success!');
-  });
-  // 其他的資料庫操作，位置預留
-  conn.query("SELECT `timestamp` FROM `user` WHERE `id`=1", function(err, result, fields){
-    if(err) throw err;
-    last_timestamp = result[0].timestamp;
-
-    if(current_timestamp/1000 > parseInt(last_timestamp)){
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    console.log("State: " + this.readyState);
-                            
-    if (this.readyState === 4) {
-    console.log("Complete.\nBody length: " + this.responseText.length);
-    access_token = JSON.parse(this.responseText).access_token;
-    var expired_timestamp = JSON.parse(this.responseText).expires_on;
-
-    var query_string = 'UPDATE `user` SET `accessToken`="'+access_token+'", `timestamp`="'+expired_timestamp+'" WHERE `id`=1';
-    conn.query(query_string, function(err, result){
-      if(err) throw err;
-      console.log(result);
-      conn.end(function(err){
-        if(err) throw err;
-        console.log('connect end');
-        })    
-      });
-      console.log('Access token retrieved successfully!');
-      res.json(access_token);
-    }
-    };                      
-    var body = 'client_id=59d86960-9f67-4980-8297-e8f2f4edb685&client_secret=yu/gwiqNAit-Pi_i/bK6n6zIOhrsAl69&grant_type=client_credentials&resource=https://graph.microsoft.com';
-    xhr.open('POST','https://login.microsoftonline.com/chen.onmicrosoft.com/oauth2/token', true);
-    xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    xhr.send(body);           
-    }
-    else{
-      const getToken = function(){
-        const promise = new Promise(function(resolve, reject){
-          conn.query("SELECT `accessToken` FROM `user` WHERE `id`=1", function(err, result, fields){
-            if(err) throw err;
-            access_token = result[0].accessToken;
-            if(access_token != null){
-              resolve(access_token);
-            }
-          })  
-        });
-          return promise;
-      };
-      
-      getToken().then(function(access_token){
-        // 關閉連線時呼叫
-      conn.end(function(err){
-        if(err) throw err;
-        console.log('connect end');
-        });
-        console.log('Access token retrieved successfully!');
-        res.json(access_token);
-      });
-//else
-      }
-  })     
-});
-
-  
-
-  
-
-
+app.use(cors());     
 
 // 新增邏輯
+app.use(express.static(path.join(__dirname, 'build')));
+  // 增加 body 解析
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:false}));
+  
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
-app.get('/addTodo',function(req,res){
-    res.send('222')
-;})
+app.get('/getEvent',function(req,res){
+      function callback(access_token){
+      fetch("https://graph.microsoft.com/beta/users/william@chen.onmicrosoft.com/calendar/events",{
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer '+ access_token,
+        'Prefer': 'outlook.timezone = "E. Australia Standard Time"'
+      },
+      }).then(res => res.json())
+      .then(
+        (result) => {
+          res.send(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          res.send(error);
+        }
+      )};
 
-// 刪除邏輯
-app.post('/removeTodo',function(req,res){
-
+      getAccessToken(callback);
+      
+  
 })
+
+app.post('/createEvent',function(req,res){
+  let date = req.body["selectedDates"][0];
+  let subject = req.body["subject"];
+  let message = req.body["message"];
+  let email = req.body["email"];
+  let name = req.body["name"];
+  function callback(access_token){
+  fetch("https://graph.microsoft.com/beta/users/william@chen.onmicrosoft.com/calendar/events",{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+ access_token
+      },
+      body: JSON.stringify({
+        "subject": subject,
+        "body": {
+          "contentType":"HTML",
+          "content": message
+        },
+        "start": {
+          "dateTime": date,
+          "timeZone": "Australian Eastern Standard Time"
+      },
+      "end": {
+          "dateTime": date,
+          "timeZone": "Australian Eastern Standard Time"
+      },
+      "attendees":[
+        {
+          "emailAddress":{
+            "address": email,
+            "name": name
+          },
+        }
+      ]
+      })
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+    }
+    getAccessToken(callback);
+  res.status(200).json("Event created!");
+});
+
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+function getAccessToken (callback){
+  
+  //Get accessToken
+    var last_timestamp;
+    var current_timestamp = Date.parse(new Date());
+    var access_token;
+  // 建立連線
+  var conn = mysql.createConnection({
+    host : '127.0.0.1',
+    user : 'root',
+    password : 'a2370307',
+    database : 'IFN702'
+    });
+    // 建立連線後不論是否成功都會呼叫
+    conn.connect(function(err){
+    if(err) throw err;
+    console.log('connect success!');
+    });
+    // 其他的資料庫操作，位置預留
+    conn.query("SELECT `timestamp` FROM `user` WHERE `id`=1", function(err, result, fields){
+      if(err) throw err;
+      last_timestamp = result[0].timestamp;
+  
+      if(current_timestamp/1000 > parseInt(last_timestamp)){
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+      console.log("State: " + this.readyState);
+                              
+      if (this.readyState === 4) {
+      console.log("Complete.\nBody length: " + this.responseText.length);
+      access_token = JSON.parse(this.responseText).access_token;
+      var expired_timestamp = JSON.parse(this.responseText).expires_on;
+  
+      var query_string = 'UPDATE `user` SET `accessToken`="'+access_token+'", `timestamp`="'+expired_timestamp+'" WHERE `id`=1';
+      conn.query(query_string, function(err, result){
+        if(err) throw err;
+        console.log(result);
+        conn.end(function(err){
+          if(err) throw err;
+          console.log('connect end');
+          })    
+        });
+        console.log('Access token retrieved successfully!');
+        callback(access_token);
+      }
+      };                      
+      var body = 'client_id=59d86960-9f67-4980-8297-e8f2f4edb685&client_secret=yu/gwiqNAit-Pi_i/bK6n6zIOhrsAl69&grant_type=client_credentials&resource=https://graph.microsoft.com';
+      xhr.open('POST','https://login.microsoftonline.com/chen.onmicrosoft.com/oauth2/token', true);
+      xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+      xhr.send(body);        
+      }
+      else{
+        const getToken = function(){
+          const promise = new Promise(function(resolve, reject){
+            conn.query("SELECT `accessToken` FROM `user` WHERE `id`=1", function(err, result, fields){
+              if(err) throw err;
+              access_token = result[0].accessToken;
+              if(access_token != null){
+                resolve(access_token);
+              }
+            })  
+          });
+            return promise;
+        };
+        
+        getToken().then(function(access_token){
+          // 關閉連線時呼叫
+        conn.end(function(err){
+          if(err) throw err;
+          console.log('connect end');
+          });
+          console.log('Access token retrieved successfully!');
+          callback(access_token);
+        });
+        }
+        
+    })
+  }
 
 // 監聽 port
 var port = process.env.PORT || 3001;
--- src/Calendar.js
+++ src/Calendar.js
@@ -7,6 +7,7 @@ import { createEvents } from './GraphService';
 import { getBookings } from './GraphService';
 import * as dateFns from 'date-fns';
 import { stringify } from 'querystring';
+import { clone } from '@babel/types';
 
 
 
@@ -15,31 +16,48 @@ import { stringify } from 'querystring';
 
 
 class Calendar extends React.Component {
-  state = {
+  constructor(props){
+    super(props);
+
+  this.state = {
     currentWeek: new Date(),
     selectedDate: new Date(),
-    selectedDates: []
+    selectedDates: [],
+    disabled: [],
+    booked: [],
+    name:'',
+    studentID: '',
+    email: '',
+    subject: 'IFN701',
+    message: '',
+    value: '',
+    mouseDown: false
   };
+  this.getEvent();
+  this.createDisabled();
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+}
+
 
   renderHeader() {
     const dateFormat = "MMMM yyyy";
-  return (
-    <div className="header row flex-middle">
-      <div className="col col-start">
-        <div className="icon" onClick={this.prevWeek}>
-          chevron_left
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={this.prevWeek}>
+            chevron_left
+          </div>
+        </div>
+        <div className="col col-center">
+          <span>{dateFns.format(this.state.currentWeek, dateFormat)}</span>
+        </div>
+        <div className="col col-end" >
+          <div className="icon" onClick={this.nextWeek}>
+            chevron_right</div>
         </div>
       </div>
-      <div className="col col-center">
-        <span>
-          {dateFns.format(this.state.currentWeek, dateFormat)}
-        </span>
-      </div>
-      <div className="col col-end" onClick={this.nextWeek}>
-        <div className="icon">chevron_right</div>
-      </div>
-    </div>
-  );
+    );
   }
 
   renderDays() {
@@ -47,116 +65,341 @@ class Calendar extends React.Component {
     const dateFormat = 'iiii';
     const days = [];
     let startDate = dateFns.startOfWeek(this.state.currentWeek);
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 8; i++) {
       //JSX syntax : push html element into array
+      if(i === 0){
+        days.push(
+          <div className="col col-center">
+            Day
+          </div>
+        )
+      }
+      else
+      {
       days.push(
         <div className="col col-center" key={i}>
-          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+          {dateFns.format(dateFns.addDays(startDate, i-1), dateFormat)}<br />
+          {dateFns.format(dateFns.addDays(startDate, i-1), "d")}
         </div>
       );
+      }
     }
     return <div className="days row">{days}</div>;
   }
 
   renderCells() {
-    const { currentWeek, selectedDate } = this.state;
+    const { currentWeek } = this.state;
     const weekStart = dateFns.startOfWeek(currentWeek);
     const weekEnd = dateFns.endOfWeek(weekStart);
     const startDate = dateFns.startOfWeek(weekStart);
     const endDate = dateFns.endOfWeek(weekEnd);
+    const today = Date.now();
 
-    const dateFormat = "d";
+    const dateFormat = "HH:mm";
     const rows = [];
-
     let days = [];
+    let sides = [];
     let day = dateFns.addHours(startDate, 9);
     let formattedDate = "";
 
-    let timeSlots = [];
-    const timeFormat = "HH:mm";
-    
+    for(let j=9; j<18; j++)
+    {
+      sides.push(
+        <div className="col cell">
+          <span className="sideText">{j}:00</span>
+        </div>
+      )
 
-    while (day <= endDate) {
+    }
+    
+    //多少個小時
+    const duration = 0.25;
+    let counter = 0;
+    //一天有9小時可以用
+    while (counter < (9/duration)*7) {
       for (let i = 0; i < 7; i++) {
-        //Create time slots
-        let formattedDay = "";
-        timeSlots = [];
-        for (let j = 0; j < 9; j++) {
-          const cloneDay = day;
-          
-          formattedDay = dateFns.format(day, timeFormat);
-          timeSlots.push(
-            <div className={` ${
-              this.state.selectedDates.find(el => el.getTime() === cloneDay.getTime()) ? "selected":""} `}
-              key={day}
-             onClick={(e) => {e.stopPropagation(); this.onTimeClick(cloneDay);}}
-             >
-               <tr>{formattedDay}</tr></div>
-          );
-          day = dateFns.addHours(day, 1);
-        }
-        day = dateFns.subHours(day, 9);
-        
-        //Create days of the week
+        const cloneDay = day;
         formattedDate = dateFns.format(day, dateFormat);
+
         days.push(
           <div
-            className={`col cell `}
+            data-date={cloneDay}
+            className={`col cell ${
+              this.state.booked.find(el => el.getTime() === cloneDay.getTime())?"booked":
+              this.handleDisabled(cloneDay)?"disabled":
+              this.state.selectedDates.find(el => el.getTime() === cloneDay.getTime())?"selected":""
+              }`}
+              onMouseDown={(e)=>this.handleClickable(e)?this.handleMouseDown(e):""}
+              onMouseOver={(e)=>this.handleClickable(e)?this.handleMouseOver(e):""}
+              onMouseUp={(e)=>this.handleClickable(e)?this.handleMouseUp(e):""}
           >
-            {/* <span className="number">{formattedDate}</span>
-            <span className="bg">{formattedDate}</span> */}
-            <table>
-              <tr>
-              <th>{formattedDate}</th>
-              </tr>
-              {timeSlots}
-            </table>
+            {/* <span className="number">{formattedDate}</span> */}
+            <span className="bg">{formattedDate}</span>
           </div>
         );
         day = dateFns.addDays(day, 1);
+        counter += 1;
+        }
+        day = dateFns.subDays(day, 7);
+        day = dateFns.addMinutes(day, duration*60);
       }
+
       rows.push(
-        <div className="row" key={day}>
+        <div className="row">
           {days}
         </div>
       );
       days = [];
-    }
-    return <div className="body">{rows}</div>;
+    return <div className="wrap"><div className="left">{sides}</div>
+    <div className="body">{rows}</div></div>;
   }
 
 
+
+  renderForm() {
+      return(
+        <form className="form" onSubmit={e => {this.handleSubmit(e)}}>
+        <ul className="form-style-1">
+            <li>
+              <label>Full Name <span className="required">*</span></label><input type="text" name="field1" className="field-divided" placeholder="First" />
+             <input type="text" name="field2" className="field-divided" placeholder="Last" onChange= {e => {this.handleChange_Name(e)}}/>
+             </li>
+             
+             <li>
+                <label>Student ID <span className="required">*</span></label>
+                <input type="text" name="field3" className="field-long" value={this.state.studentID}  onChange= {e => {this.handleChange_studentID(e)}}/>
+            </li>
+
+            <li>
+                <label>Email <span className="required">*</span></label>
+                <input type="email" name="field4" className="field-long" value={this.state.email} onChange={e => {this.handleChange_Email(e)} }/>
+            </li>
+            <li>
+                <label>Subject</label>
+                <select name="field5" value={this.state.subject} className="field-select" value={this.state.subject} onChange= {e =>{this.handleChange_Subject(e)}}>
+                <option value="IFN701">IFN701</option>
+                <option value="IFN702">IFN702</option>
+                <option value="IFN660">IFN660</option>
+                </select>
+            </li>
+            <li>
+                <label>Your Message <span className="required">*</span></label>
+                <textarea name="field6" id="field6" 
+                className="field-long field-textarea"
+                value={this.state.message} onChange={e => {this.handleChange_Message(e)} }
+                >
+                </textarea>
+            </li>
+            <li>
+                <input type="submit" value="Submit" onClick={e => {this.postEvent()}} />
+            </li>
+        </ul>
+        </form>
+      );
+
+
+    // return (
+    //   <div className="formArea">
+    //   <form className="form" onSubmit={e => {this.handleSubmit(e)}}>
+    //     <li><label>
+    //       Student ID
+    //       <input type="text" value={this.state.studentID}  onChange= {e => {this.handleChange_studentID(e)} }/>
+    //     </label></li>
+    //     <li><label>
+    //       Email
+    //       <input type="text" value={this.state.email} onChange={e => {this.handleChange_Email(e)} }/>
+    //     </label></li>
+    //     <li><label className="noteInput">
+    //       Note
+    //       <textarea type="text" value={this.state.note} onChange={e => {this.handleChange_Note(e)} }/>
+    //     </label></li>
+    //     <input type="submit" value="Submit" onClick={e => {this.postEvent()} }/>
+    //   </form>
+    //   </div>
+    // );
+  }
+
   onTimeClick = day => {
-    console.log(day+"Time");
+    console.log(day);
     let selectedDates = this.state.selectedDates;
-    console.log(selectedDates);
-    selectedDates.push(day);
+    if(selectedDates.find(el => el.getTime() === day.getTime()))
+    {
+      selectedDates = selectedDates.filter(
+        (value, index, arr)=>{return value.getTime() !== day.getTime()}
+        )
+    }
+    else
+    {
+      selectedDates.push(day);
+    }
     this.setState({
       selectedDate: day,
       selectedDates: selectedDates
     });
   };
+  
+  handleMouseDown(e){
+    let date = new Date(e.currentTarget.getAttribute('data-date'));
+    this.onTimeClick(date);
+    this.setState({mouseDown:true});
+  }
+
+  handleMouseOver(e){
+
+    let date = new Date(e.currentTarget.getAttribute('data-date'));
+    setTimeout(() => {
+      if(this.state.mouseDown){this.onTimeClick(date)};
+    }, 100); 
+  }
+
+  handleMouseUp(e){
+ 
+    this.setState({mouseDown:false});
+  }
 
   nextWeek = () => {
     this.setState({
       currentWeek: dateFns.addWeeks(this.state.currentWeek, 1)
     });
+    
+    window.requestAnimationFrame(this.getEvent.bind(this));
+    window.requestAnimationFrame(this.createDisabled.bind(this));
   };
+
   prevWeek = () => {
     this.setState({
       currentWeek: dateFns.subWeeks(this.state.currentWeek, 1)
     });
+    window.requestAnimationFrame(this.getEvent.bind(this));
+    window.requestAnimationFrame(this.createDisabled.bind(this));
   };
   
+  handleChange_Name(event) {
+    this.setState({Name: event.target.value});
+  }
+
+  handleChange_studentID(event) {
+    this.setState({studentID: event.target.value});
+  }
+  handleChange_Email(event) {
+    this.setState({email: event.target.value});
+  }
+  handleChange_Subject(event) {
+    this.setState({subject: event.target.value});
+  }
+  handleChange_Message(event) {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.email);
+    event.preventDefault();
+  }
+
+  handleDisabled(day){
+    if(this.state.disabled.find(el => el.getTime() === day.getTime()) !== undefined)
+    {
+      return true;
+    }
+      return false;
+  }
+
+  createDisabled(){
+    const { currentWeek } = this.state;
+    const weekStart = dateFns.startOfWeek(currentWeek);
+    const startDate = dateFns.startOfWeek(weekStart);
+    const today = Date.now();
+    let cloneDay = startDate;
+    let disabled = this.state.disabled;
+    while(dateFns.isBefore(dateFns.endOfDay(cloneDay), dateFns.endOfDay(today)))
+    {
+      if(this.state.booked.find(el => el.getTime() === cloneDay.getTime()) === undefined)
+      {
+        disabled.push(cloneDay);
+      }
+      cloneDay = dateFns.addMinutes(cloneDay, 15);
+    }
+    this.setState({
+      disabled: disabled
+    });
+  }
+
+  handleClickable(e){
+    let day = new Date(e.target.getAttribute('data-date'));
+    let clickable = this.state.disabled.find(el => el.getTime() === day.getTime());
+    clickable = this.state.booked.find(el => el.getTime() === day.getTime());
+  
+    if(clickable===undefined){
+      return true;
+    }
+      return false;
+  }
   render() {
     return (
       <div className="calendar">
         {this.renderHeader()}
         {this.renderDays()}
         {this.renderCells()}
+        {this.renderForm()}
       </div>
     );
   }
+  getEvent =() =>{
+    fetch("http://localhost:3001/getEvent",{
+      method: 'GET',
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          let booked = this.state.booked;
+          result.value.forEach(element => {
+            let day = new Date(element.start.dateTime);
+            booked.push(day)
+          });
+
+
+          this.setState({
+            booked: booked
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  };
+  
+  postEvent = () =>{
+    fetch("http://localhost:3001/createEvent",{
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "selectedDates" : this.state.selectedDates,
+        "name": this.state.name,
+        "studentID": this.state.studentID,
+        "email": this.state.email,
+        "subject": this.state.subject,
+        "message": this.state.message
+      })
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  };
 
 }
 
